3e14906625cb26e675a66e3b8ff5e6575b295958
Alex Barghi <abarghi@abarghi.com>
2019-10-18 15:11:43 -0400

    remove old file

--- run.sh
+++
@@ -1,5 +0,0 @@
-for k in 1 2 3 4 5 6 7 8 9 10
-do
-	./components.exe /cygdrive/c/Users/Alex/Downloads/facebook_combined.txt 2>> err.txt
-	#cmd C:/Tinkerpop/apache-gremlin-console-3.3.4/bin/gremlin.bat -i benchmark/tinkergraph.groovy 2>> err.txt
-done
