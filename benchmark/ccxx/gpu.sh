38def5ec923c67ce23c6bd28db4386f1b924d0f2
Alexandria (Alex) Barghi <bgamer50@gmail.com>
2021-12-14 12:03:10 -0500

    minor benchmark fix

--- benchmark/ccxx/gpu.sh
+++ benchmark/ccxx/gpu.sh
@@ -1,2 +1,2 @@
-../../bin/components_bchm.exe ../../data/facebook.txt cpu > out/results_cpu.txt
-../../bin/components_bchm.exe ../../data/facebook.txt gpu > out/results_gpu.txt
+../../bin/components_bchm.exe ../../data/facebook.txt cpu 2> out/results_cpu.txt
+../../bin/components_bchm.exe ../../data/facebook.txt gpu 2> out/results_gpu.txt
