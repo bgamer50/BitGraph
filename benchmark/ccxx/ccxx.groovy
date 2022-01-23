f31b65e0bdb9309ea7eee792e5a3041ae2236501
Alexandria (Alex) Barghi <bgamer50@gmail.com>
2022-01-23 18:35:03 -0500

    indexing fix

--- benchmark/ccxx/ccxx.groovy
+++ benchmark/ccxx/ccxx.groovy
@@ -31,10 +31,10 @@ while(s.hasNextInt()) {
 	i = s.nextInt().toString();
 	j = s.nextInt().toString();
 	if(!names.contains(i)) v1 = g.addV(LABEL_V).property(NAME, i).next();
-	else v1 = g.V().has(NAME, i).next();
+	else v1 = g.V().has(LABEL_V, NAME, i).next();
 
 	if(!names.contains(j)) v2 = g.addV(LABEL_V).property(NAME, j).next();
-	else v2 = g.V().has(NAME, j).next();
+	else v2 = g.V().has(LABEL_V, NAME, j).next();
 
 	names.add(i);
 	names.add(j);
@@ -63,7 +63,7 @@ while(diff > 0) {
         where("cc", neq("old_cc")).
         count().
         next()
-    println("diff: " diff.toString());
+    println("diff: " + diff.toString());
 }
 end = now()
 println('done')
--- benchmark/deg/deg.groovy
+++ benchmark/deg/deg.groovy
@@ -31,10 +31,10 @@ while(s.hasNextInt()) {
 	i = s.nextInt().toString();
 	j = s.nextInt().toString();
 	if(!names.contains(i)) v1 = g.addV(LABEL_V).property(NAME, i).next();
-	else v1 = g.V().has(NAME, i).next();
+	else v1 = g.V().has(LABEL_V, NAME, i).next();
 
 	if(!names.contains(j)) v2 = g.addV(LABEL_V).property(NAME, j).next();
-	else v2 = g.V().has(NAME, j).next();
+	else v2 = g.V().has(LABEL_V, NAME, j).next();
 
 	names.add(i);
 	names.add(j);
