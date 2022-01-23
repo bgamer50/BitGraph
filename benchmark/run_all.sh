1e332645d4db3605ec09776ff909b8c14eedd2b6
Alexandria (Alex) Barghi <bgamer50@gmail.com>
2022-01-23 17:08:50 -0500

    benchmark fixes

---
+++ benchmark/run_all.sh
@@ -0,0 +1,22 @@
+pushd ast
+./ast.sh
+popd
+
+pushd ccxx
+./facebook.sh
+./twitter.sh
+popd
+
+pushd deg
+./facebook.sh
+./twitter.sh
+popd
+
+pushd dos
+./facebook.sh
+./twitter.sh
+popd
+
+pushd lca
+./ast.sh
+popd
