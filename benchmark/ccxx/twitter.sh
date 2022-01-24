0dcc80a7ce7c430def427b25d4057280fc72c632
Alexandria (Alex) Barghi <bgamer50@gmail.com>
2022-01-23 19:15:07 -0500

    shell file fixes

--- benchmark/ccxx/twitter.sh
+++ benchmark/ccxx/twitter.sh
@@ -2,5 +2,5 @@ mkdir -p out
 ../../bin/components_bchm.exe ../../data/twitter.txt cpu 2>> out/results_cpu_twitter.txt
 ../../bin/components_bchm.exe ../../data/twitter.txt gpu 2>> out/results_gpu_twitter.txt
 python3 ccxx_cugraph.py ../../data/twitter.txt >> out/results_cugraph_twitter.txt
-/opt/gremlin/bin/gremlin.sh -i ./ccxx.groovy /root/bitgraph/data/twitter.txt 2>> neo4j out/results_neo4j_twitter.txt
-/opt/gremlin/bin/gremlin.sh -i ./ccxx.groovy /root/bitgraph/data/twitter.txt 2>> tinkergraph out/results_tinkergraph_twitter.txt
+/opt/gremlin/bin/gremlin.sh -i ./ccxx.groovy /root/bitgraph/data/twitter.txt neo4j 2>> out/results_neo4j_twitter.txt
+/opt/gremlin/bin/gremlin.sh -i ./ccxx.groovy /root/bitgraph/data/twitter.txt tinkergraph 2>> out/results_tinkergraph_twitter.txt
