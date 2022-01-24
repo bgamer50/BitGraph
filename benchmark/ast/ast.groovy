8b3820fa214580e843485144134643e978762f84
Alexandria (Alex) Barghi <bgamer50@gmail.com>
2022-01-23 20:34:04 -0500

    fix variable name

--- benchmark/ast/ast.groovy
+++ benchmark/ast/ast.groovy
@@ -49,25 +49,25 @@ r = 0
 while(r < tries) {
   // Traversal 1: Find nodes whose grandparent is a class template specialization and get their types.
   start = now()
-  r = g.V().as('s').out().dedup().out().dedup().has('INFO', "ClassTemplateSpecializationDecl").select("s").values('INFO').toList()
+  res = g.V().as('s').out().dedup().out().dedup().has('INFO', "ClassTemplateSpecializationDecl").select("s").values('INFO').toList()
   end = now()
-  println(r)
+  println(res)
   elapsed = end - start
   perror('Traversal 1 time: ' + (elapsed/1000.0).toString())
 
   // Traversal 2: Find the unique types of nodes between a Class template specialization and a namespace declaration
   start = now()
-  r = g.V().has('INFO','ClassTemplateSpecialization').out().as('t').dedup().out().dedup().has('INFO','NamespaceDecl').select('t').values('INFO').dedup().toList()
+  res = g.V().has('INFO','ClassTemplateSpecialization').out().as('t').dedup().out().dedup().has('INFO','NamespaceDecl').select('t').values('INFO').dedup().toList()
   end = now()
-  println(r)
+  println(res)
   elapsed = end - start
   perror('Traversal 2 time: ' + (elapsed/1000.0).toString())
 
   // Traversal 3: For each while loop, count the number of if statements under the while loop
   start = now()
-  r = g.V().has('INFO','WhileStmt').as('w').repeat(__.in()).emit(has('INFO','IfStmt')).select('w').values('NAME').groupCount().toList()
+  res = g.V().has('INFO','WhileStmt').as('w').repeat(__.in()).emit(has('INFO','IfStmt')).select('w').values('NAME').groupCount().next()
   end = now()
-  println(r)
+  println(res)
   elapsed = end - start
   perror('Traversal 3 time: ' + (elapsed/1000.0).toString())
 
